8a1036ca6653d3feb24aa9541b9bbefdf0d274fa
Yuval Kogman <nothingmuch@woobling.org>
2008-12-22 06:40:29 +0200

    boilerplate

--- lib/KiokuDB/Backend/DBI.pm
+++ lib/KiokuDB/Backend/DBI.pm
@@ -527,4 +527,18 @@ schema emitted by L<SQL::Translator> will not work with the queries used.
 
 =back
 
-=over
+=head1 VERSION CONTROL
+
+L<http://github.com/nothingmuch/kiokudb-backend-dbi/>
+
+=head1 AUTHOR
+
+Yuval Kogman E<lt>nothingmuch@woobling.orgE<gt>
+
+=head1 COPYRIGHT
+
+	Copyright (c) 2008 Yuval Kogman. All rights reserved
+	This program is free software; you can redistribute
+	it and/or modify it under the same terms as Perl itself.
+
+=cut
